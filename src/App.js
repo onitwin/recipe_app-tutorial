f8f104eb6f137494555b5b52f940e7631d6e2ed8
onitwin <onitwin@gmail.com>
2020-10-02 20:57:58 +0100

    added first fetch method and use effect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 const App=() =>{
+  
 
-  const exampleRequest=`https:api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+
+
+  useEffect(()=>{
+    getRecipes()
+
+  },[]) //empty array means only runs once, no array refreshes each time. alternatively add what you want to track.
+
+  const getRecipes= async()=>{
+    const response =await fetch(`https:api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data= await response.json()
+    console.log(data)
+
+  }
+
+  // alternative fetch method:
+  // fetch(`https:api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+  // .then(response =>{response.json()})
+ // .then (array.push(response))
 
 
 
   return (
     <div className="App">
-    <h1>hello</h1>
-
+      <form className='search-form' >
+        <input className="search-bar" type="text"></input>
+        <button  className="search-button" type="submit">submit</button>
+      </form>
     </div>
   );
 }
